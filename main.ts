846082c687238e0292ea387ad859b64e596ab9bb
Matthew Campbell <m.campbell92@gmail.com>
2026-01-07 12:36:51 +0100

    add temporary test call

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
 import "./style.css";
+
+import { fetchAccessToken, fetchPlaylist } from "./api";
+
+(async () => {
+  try {
+    const token = await fetchAccessToken();
+    console.log("Access token:", token);
+    const playlist = await fetchPlaylist();
+    console.log("Playlist", playlist);
+  } catch (err) {
+    console.error("Failed to fetch token", err);
+  }
+})();
