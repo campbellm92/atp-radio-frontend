b5ae3d4d38e5adec0277bcbed8a23f2fef7fa076
Matthew Campbell <m.campbell92@gmail.com>
2026-01-08 18:28:53 +0100

    refactor with async/await syntax

--- initialiseSpotifySDK.ts
+++ initialiseSpotifySDK.ts
@@ -1,25 +1,38 @@
 let player: any;
 let deviceId: string | undefined;
 
-export function initialiseSpotifyPlayer(
+export async function initialiseSpotifySDK(
   token: string,
   onReady: (deviceId: string) => void,
   onStateChange: (state: any) => void
 ) {
-  window.onSpotifyWebPlaybackSDKReady = () => {
-    player = new Spotify.Player({
-      name: "My Radio Player",
-      getOAuthToken: (cb: (token: string) => void) => cb(token),
-      volume: 0.5,
-    });
+  return new Promise<void>((resolve, reject) => {
+    window.onSpotifyWebPlaybackSDKReady = async () => {
+      player = new Spotify.Player({
+        name: "ATP Radio Player",
+        getOAuthToken: (cb: (token: string) => void) => cb(token),
+        volume: 0.5,
+      });
 
-    player.addListener("ready", ({ device_id }: { device_id: string }) => {
-      deviceId = device_id;
-      onReady(device_id);
-    });
+      player.addListener("ready", ({ device_id }: { device_id: string }) => {
+        deviceId = device_id;
+        onReady(device_id);
+      });
 
-    player.addListener("player_state_changed", onStateChange);
+      player.addListener("player_state_changed", onStateChange);
 
-    player.connect();
-  };
+      try {
+        const success = await player.connect();
+
+        if (!success) {
+          reject("Failed to connect to Spotify player");
+          return;
+        }
+        console.log("Successfully connected to the Spotify player!");
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    };
+  });
 }
