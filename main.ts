4e4a040533c3295527932d959499fd1398fb73cb
Matthew Campbell <m.campbell92@gmail.com>
2026-01-09 18:41:33 +0100

    orchestrate app logic in main

--- main.ts
+++ main.ts
@@ -1,14 +1,47 @@
 import "./style.css";
-
-import { fetchAccessToken, fetchPlaylist } from "./api";
+import { fetchAccessToken } from "./api/auth";
+import { fetchPlaylist } from "./api/playlist";
+import { initialiseSpotifySDK } from "./spotify/spotifySDK";
+import {
+  initialiseRadioController,
+  bindPlaybackControls,
+  handleStateChange,
+} from "./spotify/radioController";
 
 (async () => {
-  try {
-    const token = await fetchAccessToken();
-    console.log("Access token:", token);
-    const playlist = await fetchPlaylist();
-    console.log("Playlist", playlist);
-  } catch (err) {
-    console.error("Failed to fetch token", err);
+  const accessToken = await fetchAccessToken();
+
+  if (!accessToken) {
+    console.error("No Spotify access token found");
+    return;
   }
+
+  const playlist = await fetchPlaylist();
+  if (!playlist) {
+    console.error("Playlist could not be found");
+    return;
+  }
+
+  await startApp(accessToken, playlist);
+
+  console.log("Starting playback with URIs:", playlist);
 })();
+
+async function startApp(token: string, playlist: string[]) {
+  initialiseRadioController(token, playlist);
+  bindPlaybackControls();
+
+  await initialiseSpotifySDK(
+    token,
+    async (deviceId) => {
+      console.log("Spotify device ready:", deviceId);
+
+      const playToggleBtn = document.querySelector(
+        ".play-toggle-button"
+      ) as HTMLButtonElement | null;
+
+      if (playToggleBtn) playToggleBtn.disabled = false;
+    },
+    handleStateChange
+  );
+}
