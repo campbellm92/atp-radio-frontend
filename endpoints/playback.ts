fe1cee08b118bed70d9c1e6d25323373952debca
Matthew Campbell <m.campbell92@gmail.com>
2026-01-11 11:55:06 +0100

    add endpoints for going to previous and next songs

--- endpoints/playback.ts
+++ endpoints/playback.ts
@@ -4,7 +4,7 @@ export async function initialisePlayback(
   deviceId: string,
   uris: string[]
 ) {
-  await fetch(
+  const response = await fetch(
     `https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`,
     {
       method: "PUT",
@@ -15,4 +15,40 @@ export async function initialisePlayback(
       body: JSON.stringify({ uris }),
     }
   );
+  if (!response.ok) {
+    const error = await response.text();
+    console.error("Playback init failed:", response.status, error);
+  }
+}
+
+export async function goToPrevious(token: string, deviceId: string) {
+  const response = await fetch(
+    `https://api.spotify.com/v1/me/player/previous?device_id=${deviceId}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  if (!response.ok) {
+    const error = await response.text();
+    console.error("Playback init failed:", response.status, error);
+  }
+}
+
+export async function goToNext(token: string, deviceId: string) {
+  const response = await fetch(
+    `https://api.spotify.com/v1/me/player/next?device_id=${deviceId}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  if (!response.ok) {
+    const error = await response.text();
+    console.error("Playback init failed:", response.status, error);
+  }
 }
